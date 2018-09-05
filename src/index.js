b89257b07f991508630ccc2d51d06e541a247f1c
Alex Swan <alex@bold-it.com>
2018-09-04 23:27:33 -0500

    line/circle collision, 128bpm

--- src/index.js
+++ src/index.js
@@ -2,36 +2,80 @@ import './css/styles.css'
 import './js/kontra'
 import { Note, Sequence } from 'tinymusic'
 
+// set the playback tempo (120 beats per minute)
+var tempo = 128;
+
+function beatsToFrames(beats) {
+    // beats / bpm * seconds * frames
+    return beats / tempo * 60 * 60;
+}
+
 function degreesToRadians(degrees) {
   return degrees * Math.PI / 180;
+}
+
+function lerp (min, max, t) {
+    return min * (1-t) + max * t
+}
+
+function damp (a, b, lambda, dt) {
+    return lerp(a, b, 1 - Math.exp(-lambda * dt))
+}
+
+function circleCollidesWith(object) {
+    // assumes object is a circle
+    let r = this.radius || this.height/2;
+    let dx = this.x - object.x;
+    let dy = this.y - object.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    return distance < r + object.radius;
+}
+
+function lineCollidesWith(object) {
+    // assume object is a circle
+    // first do a circle collision to check height
+    if (!circleCollidesWith.call(this, object)) return false
+
+    // find the shortest distance to a line
+    // slope a/b is also rotation 
+    let a = Math.cos(degreesToRadians(this.rotation)) / Math.sin(degreesToRadians(this.rotation))
+    let b = 1
+    // line ax + by + c = 0
+    let c = -1 * (a * this.x + this.y)
+
+    let x0 = object.x
+    let y0 = object.y
+
+    let d = Math.abs(a*x0 + b*y0 + c) / Math.sqrt(a*a+b*b)
+    return d < this.width/2 + object.radius
 }
 
 // create a new Web Audio API context
 var ac = new AudioContext(),
     when = ac.currentTime;
 
-// set the playback tempo (120 beats per minute)
-var tempo = 64;
 
 // create some notes ('<Note Name> <Beat Length>')
 // q = quarter note, h = half note (more on that later)
-var drone = ['F4 76'],
+var drone = ['F4 ' + 16*8],
     run = [
-    '- w' // Start with 4 beats of silence
+    '- 4' // Start with 4 beats of silence
 ],
     bass = [
-    'F2 5',
-    'Eb2 5',
-    'Bb1 5',
-    'Db2 6',
-    'F2 5', // Repeated twice
-    'Eb2 5',
-    'Bb1 5',
-    'Db2 6',
-    'F2 5',
-    'Eb2 5',
-    'Bb1 5',
-    'Db2 6'
+    '- 32',
+    'F2 8',
+    'Eb2 8',
+    'Bb1 8',
+    'Db2 8',
+    'F2 8', // Repeated twice
+    'Eb2 8',
+    'Bb1 8',
+    'Db2 8',
+    'F2 8',
+    'Eb2 8',
+    'Bb1 8',
+    'Db2 8'
 ], runNotes = [
     'F3 ',
     'Ab3 ',
@@ -47,33 +91,68 @@ var drone = ['F4 76'],
 
 let factories = [];
 let sprites = [];
+
+var spinnyLine = {
+    type: 'enemy',
+    width: 35,
+    height: 90,
+    color: '#909',
+    ttl: Infinity,
+    x: 1280/2,
+    y: 720/2,
+    rotation: 0,
+    ticks: 0,
+    collidesWith: lineCollidesWith,
+    update: function (dt) {
+        this.ticks++;
+        this.rotation += ( this.ticks > beatsToFrames(36) ) ? 64/tempo : 64/tempo/2;
+
+        this.height += 2.1;
+        if (this.ticks > beatsToFrames(4)) this.height = 1800;
+        this.advance()
+    },
+    render: function () {
+        this.context.save();
+        this.context.translate(this.x, this.y);
+        this.context.rotate(degreesToRadians(this.rotation));
+        this.context.fillRect(-1*this.width/2, -this.height/2, this.width, this.height);
+        this.context.restore();
+    }
+}
+
 // Speed up the run over time
 var tearFactory = kontra.sprite({
     tears: [],
     type: 'factory',
     ttl: Infinity,
     start: function () {
-        this.tears = runSeq.notes.map(note => note.duration*60/64)
-        console.log("tearfactory start", this.tears)
+        this.tears = runSeq.notes.map(note => note.duration*60/tempo)
         factories.push(this)
     },
     update: function (dt) {
         this.tears[0] -= dt
         while (this.tears.length && this.tears[0] <= 0) {
-            let tear = kontra.sprite({
-                type: 'enemy',
-                width:6,
-                height:6,
-                color: '#909',
-                dx: Math.random()*12-6,
-                ddy:.2,
-                x:kontra.canvas.width/2,
-                y:kontra.canvas.height/2,
-                ttl: 49
-            })
-            sprites.push(tear)
+            for (let i = -1; i < 2; i=i+2) {
+                let tear = kontra.sprite({
+                    type: 'enemy',
+                    width:12,
+                    height:12,
+                    color: '#909',
+                    dx: i * (Math.random()*6+3),
+                    ddy:.3,
+                    x: (i*30) + kontra.canvas.width/2-6,
+                    y:kontra.canvas.height/2-6,
+                    ttl: 49,
+                    update: function (dt) {
+                        this.advance()
+                    }
+                })
+                sprites.push(tear)
+            }
             let timeRemaining = this.tears.shift()
             this.tears[0] -= timeRemaining
+            kitty.width += Math.random()*20 + 20;
+            kitty.height += Math.random()*20 + 20;
         }
     }
 })
@@ -87,8 +166,6 @@ var droneSeq = new Sequence( ac, tempo, drone ),
     runSeq = new Sequence( ac, tempo, run),
     bassSeq = new Sequence( ac, tempo, bass)
 runSeq.staccato = 0.55;
-// tearFactory.tears = runSeq.notes.map(note => note.duration*60/64)
-console.log(tearFactory.tears)
 
 droneSeq.gain.gain.value = 0.05;
 runSeq.gain.gain.value = 0.1;
@@ -104,20 +181,20 @@ runSeq.loop = false;
 
 
 kontra.init()
-// factories.push(tearFactory)
 
 let ship = kontra.sprite({
     x: 80,
     y: 80,
-    width: 12,  // we'll use this later for collision detection
+    width: 12,
     height: 12,
+    radius: 6, // we'll use this later for collision detection
     rotation: 0,
     ttl: Infinity,
     dashFrames: 0,
     iFrames: 0,
     stunFrames: 0,
     color: 'yellow',
-    update() {
+    update: function(dt) {
         if (this.stunFrames > 0) {
             this.stunFrames--;
             this.rotation += 4;
@@ -144,7 +221,10 @@ let ship = kontra.sprite({
                         ttl: 8,
                         width:2,
                         height:2,
-                        color:'white'
+                        color:'white',
+                        update: function (dt) {
+                            this.advance()
+                        }
                     })
                     sprites.push(particle)
                 }
@@ -182,13 +262,21 @@ let ship = kontra.sprite({
 sprites.push(ship);
 
 let kitty = kontra.sprite({
-    x:kontra.canvas.width/2 - 30,
-    y:kontra.canvas.height/2 - 30,
+
+    x:kontra.canvas.width/2 - 60,
+    y:kontra.canvas.height/2 - 60,
     type: 'enemy',
-    width:60,
-    height:60,
+    width:120,
+    height:120,
     ttl: Infinity,
-    color: '#909'
+    color: '#909',
+    update: function (dt) {
+        this.width = damp(this.width, 120, 12, dt)
+        this.height = damp(this.height, 120, 12, dt)
+        this.x = (kontra.canvas.width - this.width)/2
+        this.y = (kontra.canvas.height - this.height)/2
+        this.advance()
+    }
 })
 sprites.push(kitty)
 
@@ -197,23 +285,19 @@ let conductor = kontra.gameLoop({
     fps: tempo/60,
     render: function() {},
     clearCanvas: false,
-    update: function() {
+    update: function(dt) {
         if (this.beat === undefined) { this.beat = -1 }
         this.beat++;
-        console.log("beat", this.beat);
-        if (this.beat == 0) {
+        console.log("beat " + Math.floor(1 + this.beat / 4) + '-' + Math.floor(1 + this.beat % 4)  + " (" + this.beat + ")");
+        if (this.beat === 0) {
             tearFactory.start();
             droneSeq.play();
             runSeq.play();
-        }
-        if (this.beat == 14) {
             bassSeq.play();
         }
-        // console.log("beat", this.beat)
-        // at 0 beats, start the music
-
-        // at 55, start the melody
-        // at 76 beats, start the drop
+        if (this.beat == 28 || this.beat === 36 || this.beat === 44 || this.beat === 52) {
+            sprites.push(kontra.sprite(spinnyLine))
+        }
     },
 })
 
@@ -224,16 +308,16 @@ let loop = kontra.gameLoop({  // create the main game loop
         })
         factories = factories.filter(factory => factory.isAlive());
         sprites.map(sprite => {
-            sprite.update();
+            sprite.update(dt);
             // Wrap the stage bc why not
             sprite.x = (sprite.x + kontra.canvas.width) % kontra.canvas.width
             sprite.y = (sprite.y + kontra.canvas.height) % kontra.canvas.height
-            if (!ship.iFrames && sprite.type === 'enemy' && ship.collidesWith(sprite)) {
+            if (!ship.iFrames && sprite.type === 'enemy' && sprite.collidesWith(ship)) {
                 ship.stunFrames = 45;
                 ship.iFrames = 60;
                 let angle = Math.random()*2*Math.PI;
-                ship.dx = Math.cos(angle)*4;
-                ship.dy = Math.sin(angle)*4;
+                ship.dx = Math.cos(angle)*8;
+                ship.dy = Math.sin(angle)*8;
             }
         })
         sprites = sprites.filter(sprite => sprite.isAlive());
@@ -257,6 +341,7 @@ kontra.keys.bind('space', function () {
 let startGame = function() {
     // Game
     loop.start();
+    conductor.beat = -1;
     conductor.start();
     // Music
 }
